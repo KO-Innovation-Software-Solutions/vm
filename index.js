a573f8fda3b9091b8ea86ce277d7c98a0bbecdca
Muthukumar <muthu.kumar@koinnovation.com>
2024-05-13 12:15:28 +0530

    first iupdae

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
+const express = require('express');
+const app = express();
+const port = 8021;
+const utils = require('./utils');
 const net = require('net');
+const commandutile = require('./command');
+const fs = require('fs');
+const filePath = 'data.txt';
 
 // Define the IP address and port to listen on
-const IP_ADDRESS = '115.245.218.52'; // Use '0.0.0.0' to listen on all available network interfaces
-const PORT = 8012;
+const IP_ADDRESS = '115.245.218.52'; 
+const PORT = 8021;
 
-// Create a TCP server
+
+
+
 const server = net.createServer((socket) => {
-    // Connection callback - this function is called whenever a new client connects
-    console.log('Client connected');
+    console.log('Client connected!');
 
-    // Handle incoming data from the client
+   
+
+   const command = '0x4001'; // PID for latitude
+    const long = '' 
+    // socket.write("0xF1 0x00");
+    // socket.write('0x2802');
+    socket.write(command);
+    // Handle data received from the client
     socket.on('data', (data) => {
-        console.log('Received data:', data.toString());
+       
+        commandutile.encodeOBD(data);
+       console.log(data);
+       console.log('Received data from client:', data);
+
+        // const request = data; // Replace with your actual request data
+        // const hexString = '0x4001';
+        // const rawHex = Buffer.from(hexString, 'hex');
+        // socket.write(rawHex); // Send the request to the device
+        // Echo back the received data to the client
+     
     });
+
+
+
+    // const command = 'AT+STATUS\r\n';
+    socket.write(command);
 
     // Handle client disconnection
     socket.on('end', () => {
-        console.log('Client disconnected');
+        console.log('Client disconnected!');
     });
 });
 
-// Start listening for connections
-server.listen(PORT, IP_ADDRESS, () => {
-    console.log(`Server listening on ${IP_ADDRESS}:${PORT}`);
+
+server.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
 });
